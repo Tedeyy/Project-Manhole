5cbd1ad124e4d525f8afc416d7184fd8cb197f3e
Tedeyy <tedjus666@gmail.com>
2026-05-11 11:41:30 +0800

    fixed UI bugs for project container

--- src/components/body/projects/projectBody.tsx
+++ src/components/body/projects/projectBody.tsx
@@ -8,7 +8,9 @@ function projectBody() {
     <>
       <div className="project-container">
         <div className="project">
-          <h4 className="title">Learning Management System</h4>
+          <div className="title">
+            <h4>EduReady | A Learning Management System</h4>
+          </div>
           <div className="image-container">
             <img src={lmsclient} alt="lms-client" className="project-image" />
             <img
@@ -19,8 +21,37 @@ function projectBody() {
           </div>
           <div className="description">
             <p>
-              This is a Research Project for my clients from Institute of
-              Teacher Education.
+              EduReady is an online Learning Management System that is developed
+              to help the students of the Institute of Teacher Education have a
+              better and interactive learning experience.
+              <br />
+              <br />
+              Currently the system is under development and is launched for
+              prototype testing.
+            </p>
+          </div>
+        </div>
+        <div className="project">
+          <div className="title">
+            <h4>EduReady | A Learning Management System</h4>
+          </div>
+          <div className="image-container">
+            <img src={lmsclient} alt="lms-client" className="project-image" />
+            <img
+              src={lmsscreenshot}
+              alt="lms-screenshot"
+              className="project-image"
+            />
+          </div>
+          <div className="description">
+            <p>
+              EduReady is an online Learning Management System that is developed
+              to help the students of the Institute of Teacher Education have a
+              better and interactive learning experience.
+              <br />
+              <br />
+              Currently the system is under development and is launched for
+              prototype testing.
             </p>
           </div>
         </div>
